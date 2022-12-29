ba369e10ca413068d9b6f3db4827021fa12b2a48
AhmadMuzayyin <muzayyin654@gmail.com>
2022-12-30 06:13:23 +0700

    update laptop

--- resources/views/pageBackEnd/request/export.blade.php
+++ resources/views/pageBackEnd/request/export.blade.php
@@ -1,22 +1,43 @@
 <table>
-    <thead>
-        <tr>
-            <th>Batch Code</th>
-            <th>Nama Produk</th>
-            <th>SN</th>
-            <th>PIN</th>
-            <th>Link</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($data as $key => $dt)
+    @if (!$batch_code->isEmpty())
+        <thead>
             <tr>
-                <td>{{ $batch_code[$key]->batch_code }}</td>
-                <td>{{ $dt->nama_produk }}</td>
-                <td>{{ $dt->serial_number }}</td>
-                <td>{{ $dt->pin}}</td>
-                <td>{{ url('/scan',$dt->serial_number); }}</td>
+                <th>Batch Code</th>
+                <th>Nama Produk</th>
+                <th>SN</th>
+                <th>PIN</th>
+                <th>Link</th>
             </tr>
-        @endforeach
-    </tbody>
+        </thead>
+        <tbody>
+            @foreach ($data as $key => $dt)
+                <tr>
+                    <td>{{ $batch_code[$key]->batch_code }}</td>
+                    <td>{{ $dt->nama_produk }}</td>
+                    <td>{{ $dt->serial_number }}</td>
+                    <td>{{ $dt->pin }}</td>
+                    <td>{{ url('/scan', $dt->serial_number) }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    @else
+        <thead>
+            <tr>
+                <th>Nama Produk</th>
+                <th>SN</th>
+                <th>PIN</th>
+                <th>Link</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($data as $key => $dt)
+                <tr>
+                    <td>{{ $dt->nama_produk }}</td>
+                    <td>{{ $dt->serial_number }}</td>
+                    <td>{{ $dt->pin }}</td>
+                    <td>{{ url('/scan', $dt->serial_number) }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    @endif
 </table>
